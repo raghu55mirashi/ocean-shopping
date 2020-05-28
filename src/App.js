afb203d6be0b248008a282b2e22a1c56ee463427
raghu55mirashi <raghu55mirashi@gmail.com>
2020-05-28 12:46:45 +0530

    added routing functionality with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage'
+import Error from './components/error/error'
+
+const Hats = (props) => {
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  )
+}
+const Jackets = (props) => {
+  return (
+    <div>
+      <h1>Jackets</h1>
+    </div>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={Hats} />
+        <Route exact path="/jackets" component={Jackets} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
+
 import './menu-item.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`} >
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+    <div className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image"
             style={{ backgroundImage: `url(${imageUrl})` }} />
         <div className="content">
@@ -12,4 +15,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
